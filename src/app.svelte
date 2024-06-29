97471525777aee6e215c6139a384335f35609ee8
Ivan Tusnolobov <smooth.wheel1951@tsn.im>
2024-06-29 22:11:21 +0800

    Quick Save

--- src/app.svelte
+++ src/app.svelte
@@ -1,1 +1,35 @@
-<div>123</div>
+<script>
+    import Input from "./input.svelte";
+    import Item from "./item.svelte";
+    import { makeStorage } from "./utils";
+
+    let storage = makeStorage("todos");
+    let todos = $state(storage.get() ?? []);
+
+    $effect(() => {
+        storage.set(todos);
+    });
+
+    function add(newTodo) {
+        todos.push(newTodo);
+    }
+
+    function remove(id) {
+        todos = todos.filter((todo) => todo.id !== id);
+    }
+
+    function edit(updatedTodo) {
+        let index = todos.findIndex((todo) => todo.id === updatedTodo.id);
+        todos[index] = updatedTodo;
+    }
+</script>
+
+<div class="pt-16 max-w-sm mx-auto grid gap-6">
+    <h1 class="font-bold text-3xl pb-2">Todo Demo Svelte</h1>
+    <Input {add} />
+    <ul class="grid gap-3">
+        {#each todos as todo (todo.id)}
+            <Item {todo} {remove} {edit} />
+        {/each}
+    </ul>
+</div>
---
+++ src/input.svelte
@@ -0,0 +1,24 @@
+<script>
+    import { makeID } from "./utils";
+    let { add } = $props();
+    let value = $state("");
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        let newText = value.trim();
+        if (newText) {
+            add({ id: makeID(), text: newText });
+            value = "";
+        }
+    }
+</script>
+
+<form onsubmit={handleSubmit} class="flex gap-2">
+    <input
+        bind:value
+        type="text"
+        placeholder="Add a new task..."
+        class="px-4 py-2 border rounded-md text-sm flex-1"
+    />
+    <button class="px-4 py-2 bg-[#ff3e00] text-white rounded-md text-sm font-medium"> Add </button>
+</form>
